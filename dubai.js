1147326181ac2d26ed330efb8c543b980e4975fa
Nathan141007 <134866739+Nathan141007@users.noreply.github.com>
2023-05-28 13:47:22 -0400

    Add files via upload

---
+++ dubai.js
@@ -0,0 +1,13 @@
+
+function img1() {
+    document.getElementById("cambio").src= "burj.png" ;
+}
+function img2(){
+    document.getElementById("cambio").src="j.png";
+}
+function img3(){
+    document.getElementById("cambio").src= "des.png";
+}
+function img4(){
+    document.getElementById("cambio").src= "dw.png";
+}
